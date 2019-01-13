acd05e85ea3699358c64784b4cfef935fd5bd6a1
Santiago Cujaban <santiero12@gmail.com>
2019-01-13 18:01:54 -0500

    rewritten and documented script.js

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-function addtext() {
-  var padre = document.getElementById("tareas");
+window.onload = () => {
+  var addbutton = document.getElementById("addbutton"); // button to add inputs
+
+  addbutton.onclick = addinputs; // when is onclick runs addinputs function
+}
+
+function addinputs() {
+
+  var padre = document.getElementById("homeworks"); // parent
   
   var input1 = document.createElement("INPUT");
   input1.type = "checkbox";
+
+  input1.onclick = function() { // to check if text is empty
+    if(input2.value == false) {
+      alert("¡Agrega contenido a tu tarea!");
+      input1.checked = false;
+    } else {
+      alert("¡Felicidades, haz completado tu tarea!");
+    }
+  }
+
   padre.appendChild(input1);
  
   var input2 = document.createElement("INPUT");
   input2.type = "text";
   input2.placeholder = "Indica aquí tu tarea";
+
   padre.appendChild(input2);
   
-  var del = document.createElement("INPUT");
+  var del = document.createElement("INPUT"); // button to delete inputs
   del.type = "button";
   del.value = "x";
   
@@ -21,23 +39,7 @@ function addtext() {
   }
   
   padre.appendChild(del);
-  
-  var salto = document.createElement("p");
-  
-  input1.onclick = function() {
-    if(input2.value == false) {
-      alert("¡Agrega contenido a tu tarea!");
-      input1.checked = false;
-    } else {
-      alert("¡Felicidades, haz completado tu tarea!");
-    }
-  }
-  
-  padre.appendChild(salto);
-}
 
-window.onload = function() {
-  var addbutton = document.getElementById("addbutton");
-  
-  addbutton.onclick = addtext;
+  var salto = document.createElement("p"); // line break between input groups
+  padre.appendChild(salto);
 }
